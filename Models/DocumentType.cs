96fdda564bb0d245c96b534f7bdb2ed9853da1b7
Camilobarre <barrecami@gmail.com>
2024-08-26 07:53:43 -0500

    feat: DataAnotation was created in the class "DocumentType"

--- Models/DocumentType.cs
+++ Models/DocumentType.cs
@@ -14,10 +14,14 @@ namespace LibrarySystem.Models
         [Column("id")]
         public int Id { get; set; }
         [Column("name")] //Asi se ponen las columnas en la tabla
+        [MaxLength(50, ErrorMessage = "The Name field must be less than {50} character.")] //Condiciones de máximos caracteres
         public required string Name { get; set; }
         [Column("abbreviation")]
+        [MinLength(2, ErrorMessage = "The Abbreviation field must be more than {2} character.")] //Condiciones de mínimos caracteres
+        [MaxLength(10, ErrorMessage = "The Abbreviation field must be less than {10} character.")]
         public required string Abbreviation { get; set; }
         [Column("description")]
+        [MaxLength(500, ErrorMessage = "The Description field must be less than {500} character.")]
         public string? Description { get; set; }
     }
 }
